e071875001e4d3e10b97f5e91e5c397adf071d63
Jonghyun-K1m <jonghyun424@naver.com>
2019-02-06 15:21:20 +0900

    commit

--- test.java
+++ test.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
 public class test{
 
     public static void main(String args[]){
+        
+        Scanner aScanner = new Scanner(System.in);
+        String a;
 
-        System.out.println("Sdss");
-         
+        System.out.println("input");
+        a=aScanner.nextLine();
+        System.out.println("output"+a);
 
+        aScanner.close();
 
     }
 }
