16409c6f7caa880cb8af67a492c18540b4210964
Jonghyun-K1m <jonghyun424@naver.com>
2019-02-12 17:31:13 +0900

    com

--- h0212/p05.java
+++ h0212/p05.java
@@ -1,26 +1,53 @@
 package h0212;
 import java.util.*;
 
+//import com.sun.org.apache.xpath.internal.operations.String;
+class point{
+    public int p,q;
+    public void move(int a,int b){
+        p=a;
+        q=b;
+    }
+    public String toString(){
+        return " "+p+" "+q+" (의 점)";
+    }
+}
+class Positivepoint extends point{
+
+    public void move(int a,int b){
+        if(a>0 && b>0){
+           super.move(a,b);
+        }
+        
+    }
+    Positivepoint(int a,int b){
+        if(a>0 && b>0){
+            p=0;
+            q=0;
+        }
+    }
+    Positivepoint(){
+    }
+}
 
 
-public class p01 {
+public class p05 {
 
+   
     public static void main(String args[]){
 
-       String text;
-       int len;
-       Scanner in=new Scanner(System.in);
 
-        text=in.nextLine();
-        len=text.length();
-        for(int i=0;i<len;i++){
+        Positivepoint p=new Positivepoint();
+       p.move(10,10);
+       System.out.println(p.toString()+"입니다");
 
-            System.out.print(text.charAt(i));
-            
-        }
-
+       p.move(-5,5);
+       System.out.println(p.toString()+"입니다");
        
+       Positivepoint p2=new Positivepoint(-10,-10);
+       //p2.move(10,10);
+       System.out.println(p2.toString()+"입니다");
 
     }
 
-}
+}k
