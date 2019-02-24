9b8c524da048995c512203641afa8c3046fcf2ef
Jonghyun-K1m <jonghyun424@naver.com>
2019-02-24 16:58:27 +0900

    c

---
+++ h0224/Location.java
@@ -0,0 +1,21 @@
+package h0224;
+
+public class Location {
+	public int we;
+	public int kyung;
+	
+	public int getWe() {
+		return we;
+	}
+	public void setWe(int we) {
+		this.we = we;
+	}
+	public int getKyung() {
+		return kyung;
+	}
+	public void setKyung(int kyung) {
+		this.kyung = kyung;
+	}
+	
+	
+}
---
+++ h0224/LocationMain.java
@@ -0,0 +1,34 @@
+package chap01;
+
+import java.util.*;
+public class LocationMain {
+	
+	
+	public static void main(String args[]) {
+		
+		HashMap<String,Location> mapt=new HashMap<>();
+		Scanner in=new Scanner(System.in);
+		int input,input2;
+		String strinput;
+		for(int i=0;i<3;i++) {
+			strinput=in.next();
+			Location a=new Location();
+			a.kyung=in.nextInt();
+			a.we=in.nextInt();
+					
+			mapt.put(strinput, a);
+		}
+		
+		
+		strinput=in.next();
+		if(mapt.containsKey(strinput)) {
+			System.out.println(mapt.get(strinput).kyung);
+			System.out.println(mapt.get(strinput).we);
+			
+		}
+	}
+	
+	
+	
+	
+}
